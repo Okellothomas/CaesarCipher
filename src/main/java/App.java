7eb538f413e50a8ec7c360999c4ca2005e615a8e
Okellothomas <thomasolal33@gmail.com>
2022-04-30 21:28:27 +0300

    Add the App.java and add the neccessary imports

---
+++ src/main/java/App.java
@@ -0,0 +1,11 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Console;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
