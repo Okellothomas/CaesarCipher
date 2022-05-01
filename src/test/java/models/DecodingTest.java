8bddc291bed6ea014f2b576461ed9817ca0abca7
Okellothomas <thomasolal33@gmail.com>
2022-05-01 09:10:43 +0300

    Add and set the encoding

--- src/test/java/models/DecodingTest.java
+++ src/test/java/models/DecodingTest.java
@@ -2,10 +2,23 @@ package models;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class DecodingTest {
+
+    @Test
+    public void getInputKeys(){
+        Decoding decoding = new Decoding(1, "one");
+        assertEquals(1, decoding.getInputKey());
+    }
+
+    @Test
+    public void getUserInputs(){
+        Decoding decoding =  new Decoding(1, "one");
+        assertEquals("one", decoding.getUserInput());
+    }
 
     @Before
     public void setUp() throws Exception {
